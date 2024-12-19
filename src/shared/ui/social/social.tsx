c830c7cb602a2d00b443254cfa8c6cda9c8eb686
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-12-19 22:23:30 +0300

    fix: small changes and feat social

--- src/shared/ui/social/social.tsx
+++ src/shared/ui/social/social.tsx
@@ -1,5 +1,14 @@
 import styles from './social.module.scss';
+import type { Properties } from './social.properties';
 
-const Social = () => ('');
+const Social = (properties: Properties) => {
+  const { href, icon } = properties;
+
+  return (
+    <a className={styles.wrapper} href={href}>
+      {icon}
+    </a>
+  );
+};
 
 export { Social };
