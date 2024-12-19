5c50afa321cbef427fd32a2cb99e40aab4260d56
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-12-20 00:21:53 +0300

    fix: small changes

--- src/widgets/sidebar/sidebar.tsx
+++ src/widgets/sidebar/sidebar.tsx
@@ -1,8 +1,22 @@
+// import { CoinIcon, TerminalIcon } from '@/icons';
+// import { Title } from '@/ui';
+
+// import { ExperienceLabel } from '../experience-label/experience-label';
 import styles from './sidebar.module.scss';
 
 const Sidebar = () => (
-  <aside>
-    <span />
+  <aside className={styles.wrapper}>
+    {/* <Title title='Никита Алманов (nikkeyl)' />
+    <ExperienceLabel
+      icon={<TerminalIcon />}
+      startDate='2021-04-21'
+      text='в разработке'
+    />
+    <ExperienceLabel
+      icon={<CoinIcon />}
+      startDate='2024-08-23'
+      text='коммерческого опыта'
+    /> */}
   </aside>
 );
 
