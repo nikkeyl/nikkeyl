c7eab8d1b6ce07994ccb3a5561ffc50088622090
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-12-20 23:13:29 +0300

    refactor: small changes

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('src/app/i18n/get-request-config.ts');
 
-export default withNextIntl(
-  {
-    images: {
-      formats: ['image/webp'],
-      remotePatterns: [
-        {
-          hostname: 'nikkeyl.vercel.app',
-          pathname: '/images/**',
-          protocol: 'https',
-        },
-      ],
-    },
+export default withNextIntl({
+  images: {
+    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        hostname: 'nikkeyl.vercel.app',
+        pathname: '/images/**',
+        protocol: 'https',
+      },
+    ],
   },
-);
+});
