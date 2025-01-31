305cb7ed88ec61f920d54ef9d5cc7c6006965a07
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-01-31 23:32:47 +0300

    refactor: label

--- src/widgets/experience-label/experience-label.tsx
+++ src/widgets/experience-label/experience-label.tsx
@@ -11,9 +11,10 @@ const ExperienceLabel = async (properties: Properties) => {
 
   const { months, years } = calculateExperience(new Date(startDate));
 
-  const yearsText = years > 0 ? `${years} ${getYearsDeclension(years)}` : '';
-  const monthsText = months > 0 ? `${months} ${getMonthsDeclension(months)}` : '';
-  const experienceText = [yearsText, monthsText].filter(Boolean).join(' ');
+  const yearsText = `${years} ${getYearsDeclension(years)}`;
+  const monthsText = `${months} ${getMonthsDeclension(months)}`;
+  const experienceText = `${yearsText} ${monthsText}`;
+
   const Icon = icon;
 
   return (
