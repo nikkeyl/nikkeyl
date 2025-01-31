1652c2a850273a94b80cbe8d9cc6ba69ecc3ecdb
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-01-31 22:33:18 +0300

    fix: extract

--- src/shared/helpers/extract-file-name.ts
+++ src/shared/helpers/extract-file-name.ts
@@ -2,7 +2,7 @@ const extractFileName = (path: string) => {
   const parts = path.split('/');
   const fileNameWithExtension = parts[parts.length - 1] ?? '';
 
-  return fileNameWithExtension.split('.')[0];
+  return `${fileNameWithExtension.split('.')[0]}`;
 };
 
 export { extractFileName };
