10c99ab39f25738f8b087cbb12ee1b18ef5a35ba
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-01-31 23:21:55 +0300

    refactor: get locale

--- src/app/i18n/get-locale.ts
+++ src/app/i18n/get-locale.ts
@@ -1,12 +1,11 @@
-'use server';
-
 import { headers } from 'next/headers';
 
 const getLocale = async () => {
-  const browserLocale = (await headers()).get('accept-language') ?? 'en';
+  const browserLocale = await headers();
+  const currentLocale = browserLocale.get('accept-language') ?? 'en';
   const supportedLocales = ['de', 'en', 'ja', 'ru'];
   const defineLocale =
-    browserLocale
+    currentLocale
       .split(',')
       .map((locale: string) => locale.trim())[0]
       ?.split('-')[0] ?? 'en';
