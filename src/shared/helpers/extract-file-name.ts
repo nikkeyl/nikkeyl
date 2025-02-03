df46389f443f83d418b5d89cf2f89314e1f6659d
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-02-03 09:41:42 +0300

    refactor: small changes

--- src/shared/helpers/extract-file-name.ts
+++ src/shared/helpers/extract-file-name.ts
@@ -1,10 +1,10 @@
 const extractFileName = (path: string, isClean?: boolean) => {
   const parts = path.split('/');
-  const fileNameWithExtension = parts[parts.length - 1] ?? '';
-  const fileNameWithoutExtension = fileNameWithExtension.split('.')[0] ?? '';
-  const formattedFileName = fileNameWithoutExtension.replace(/-+/g, ' ').trim();
+  const fileName = parts[parts.length - 1] ?? '';
+  const fileNameFormat = fileName.split('.')[0] ?? '';
+  const fileNameClean = fileNameFormat.replace(/-+/g, ' ').trim();
 
-  return isClean ? formattedFileName : fileNameWithoutExtension;
+  return isClean ? fileNameClean : fileNameFormat;
 };
 
 export { extractFileName };
